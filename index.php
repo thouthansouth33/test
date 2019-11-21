a26204a6555e61cfe2eb0cadd66c8a1e87e3cb7a
thouthansouth33 <nanashi77484@yahoo.co.jp>
2019-11-21 11:25:26 +0900

    qwerty

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="ja">
+	
+<head>
+<meta charset="UTF-8">
+<title>4eachblog 掲示板</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+	
+<body>
+	
+	<header>
+		<div class="logo"></div>
+	</header>
+	
+	<img src="4eachblog_logo.jpg">
+	
+	<ul class="menu">
+		<li>トップ</li>
+		<li>プロフィール</li>
+		<li>4eachについて</li>
+		<li>登録フォーム</li>
+		<li>問い合わせ</li>
+		<li>その他</li>
+	</ul>
+	
+	<h1>プログラミングに役立つ掲示板</h1>
+	
+	<div class="box1">
+	 <div class="abc">入力フォーム</div>
+	
+	
+	</div>
